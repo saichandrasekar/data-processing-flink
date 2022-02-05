c6ed36834c9f338c082b6494c6edef51925b27fa
saichandrasekar <saichandrasekar@gmail.com>
2022-02-05 22:24:39 +0530

    initial draft

---
+++ src/main/java/com/garuda/datastudio/DataStudioEngineApplication.java
@@ -0,0 +1,9 @@
+package com.garuda.datastudio;
+
+public class DataStudioEngineApplication {
+
+	public static void main(String a[]) {
+		System.out.println("Data Studio - Engine Application...is alive!!");
+	}
+
+}
